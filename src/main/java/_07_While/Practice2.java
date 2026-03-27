63190107c5318d8ef75ad9426f824cf9894b298a
elikese <elikese@gmail.com>
2026-03-27 21:40:22 +0900

    feat: break, continue

--- src/main/java/_07_While/Practice2.java
+++ src/main/java/_07_While/Practice2.java
@@ -1,5 +1,7 @@
 package _07_While;
 
+import java.util.Scanner;
+
 public class Practice2 {
     public static void main(String[] args) {
         // for문과 break - 탐색할때 타겟을 찾으면 탈출
@@ -29,6 +31,40 @@ public class Practice2 {
 
         // 1~100까지 누적합을 하다가
         // 누적합이 2000이 넘어갈 시 탈출! - 그때 누적합을 출력!
+        int sum = 0;
+        for(int i = 1; i <= 100; i++) {
+            sum += i;
+
+            if(sum > 2000) {
+                System.out.println("탈출! :" + sum);
+                break;
+            }
+        }
+
+        String password = "1234";
+        Scanner scanner = new Scanner(System.in);
+        int count = 0;
+        while(true) {
+            System.out.print("비밀번호 입력 >");
+            String inputPW = scanner.nextLine();
+
+            // 비밀번호 맞추면, 문을 열었습니다! 출력 후 탈출
+            // 틀리면, 다시 입력받게!
+            // 추가조건) 5회 틀릴시, 문이 잠겼습니다! 출력 후 탈출
+            if(inputPW.equals(password)) { // 맞췄을 때
+                System.out.println("문을 열었습니다!");
+                break;
+            } else { // 틀렸을 때
+                System.out.println("틀렸습니다 다시입력!");
+                count++; // 틀리면 1 증가
+            }
+
+            if(count == 5) {
+                System.out.println("5회시도!! 문이 잠겼습니다");
+                break;
+            }
+
+        }
 
     }
 }
